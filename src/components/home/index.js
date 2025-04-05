2374299077a361a0814b257389208afbd55292d5
unknown <otgoohghg@yahoo.com>
2025-04-05 16:43:21 +0800

    543

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -32,8 +32,8 @@ export default function Home() {
         <div className="absolute top-[42%] right-[10%] z-10 w-full flex flex-col items-center">
           <nav className="flex justify-center items-center gap-2 mt-4">
             <div className="dropdown dropdown-top">
-              <label tabIndex={0} className="btn btn-outline btn-primary">A1 - A15</label>
-              <ul tabIndex={0} className="dropdown-content menu text-center rounded-box z-10 w-72 h-40 p-2 shadow-sm overflow-y-auto">
+              <label tabIndex={0} className="btn">A1 - A15</label>
+              <ul tabIndex={0} className="dropdown-content menu text-center bg-white rounded-box z-10 w-72 h-40 p-2 shadow-sm overflow-y-auto">
                 {[...Array(15).keys()].map((i) => (
                   <li key={`A${i + 1}`}>
                     <a onClick={() => handleVideoSelection(i + 1)}>A{i + 1}</a>
@@ -43,8 +43,8 @@ export default function Home() {
             </div>
 
             <div className="dropdown dropdown-top">
-              <label tabIndex={0} className="btn btn-outline btn-primary">B1 - B6</label>
-              <ul tabIndex={0} className="dropdown-content menu text-center rounded-box z-10 w-24 p-2 shadow-sm">
+              <label tabIndex={0} className="btn">B1 - B6</label>
+              <ul tabIndex={0} className="dropdown-content menu text-center bg-white rounded-box z-10 w-24 p-2 shadow-sm">
                 {[...Array(6).keys()].map((i) => (
                   <li key={`B${i + 1}`}>
                     <a onClick={() => handleVideoSelection(i + 16)}>B{i + 1}</a>
@@ -54,7 +54,7 @@ export default function Home() {
             </div>
 
             <button
-              className="btn btn-outline btn-primary"
+              className="btn"
             >
               Байршил
             </button>
